f3ff4ee8bbc40a333b0dc1b28153341b1c493eac
unknown <fancy.bubochka@gmail.com>
2025-02-02 20:35:09 +0400

    added first model and got access to admin panel django

--- app/models.py
+++ app/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+class Product(models.Model):
+
+    name = models.CharField(max_length=255)
+    price = models.IntegerField(default=0)
+    # pub_date = models.DateTimeField("date published")
+    # question = models.ForeignKey(Question, on_delete=models.CASCADE)
+
+
+    def __str__(self):
+        return self.name
